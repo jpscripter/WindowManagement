c1974d58ae8da74fa9c387c7f36dfb38cef7017d
JPScripter <JPScripter@gmail.com>
2023-04-21 19:17:37 -0500

    adding visable

--- Commands/Get-ChildWindow.ps1
+++ Commands/Get-ChildWindow.ps1
@@ -3,15 +3,23 @@
 param (
     [Parameter(ValueFromPipeline = $true, ValueFromPipelinebyPropertyName = $true)]
     [ValidateNotNullorEmpty()]
-    [System.IntPtr]$WindowHandle = [intptr]::zero
+    [System.IntPtr]$WindowHandle = [intptr]::zero,
+    [switch]$All
+
 )
+    $Windows = New-Object Collections.Arraylist 
     foreach ($child in ([Pinvoke.Logic.UserDesktopLogic]::GetChildWindows($WindowHandle))){
 
-        [PSCustomObject] @{
+        $Null = $Windows.add([PSCustomObject] @{
             WindowHandle = $WindowHandle
             ChildId = $child
             ChildTitle = (Get-WindowName -WindowHandle $child)
-            processID = Get-WindowProcess -WindowHandle $WindowHandle
-        }
+            processID = Get-WindowProcess -WindowHandle $child
+        })
+
     }
+    if (-not $all.IsPresent){
+        $Windows = $windows.where({[Pinvoke.User32]::IsWindowVisible($Psitem.CHildID)})
+    }
+    $Windows
 }
