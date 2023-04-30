01838c983cf257be8b837c6f5e41d349ba34b7e9
JPScripter <JPScripter@gmail.com>
2023-04-30 12:31:36 -0500

    update window settings

--- Commands/Set-Window.ps1
+++ Commands/Set-Window.ps1
@@ -3,21 +3,24 @@ function Set-Window {
     param
     (
       [IntPtr] $WindowHandle,
-      [Pinvoke.WindowAffinity] $Affinity,
+      #[Pinvoke.WindowAffinity] $Affinity,
       [Pinvoke.WindowShowStyle] $Style,
       [Pinvoke.zOrderOptions] $zOrder,
       [Pinvoke.RECT]$Coordinates
     )
 
+    <#
+    # This only works for current process
     if ($PSBoundParameters.ContainsKey('Affinity')){
       $Success = [Pinvoke.User32]::SetWindowDisplayAffinity($WindowHandle, $Affinity)
       if (-not $Success){
         $ErrorMessage = [System.ComponentModel.Win32Exception][System.Runtime.InteropServices.Marshal]::GetLastWin32Error()
         Write-Error $ErrorMessage
+        Write-Error $ErrorMessage
         throw "Failed setting $Affinity"
       }
     }
-
+    #>
 
     if ($PSBoundParameters.ContainsKey('Zorder')){
       $Success = [Pinvoke.User32]::SetWindowPos($WindowHandle,$zorder,0,0,0,0, [Pinvoke.WindowOptions]::SWP_NOSIZE + [Pinvoke.WindowOptions]::SWP_NOMOVE  )
