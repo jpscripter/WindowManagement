68d522618e031b1a21c96bbb46dd4d77d712830b
JPScripter <JPScripter@gmail.com>
2023-04-30 12:26:55 -0500

    style

--- Commands/Set-Window.ps1
+++ Commands/Set-Window.ps1
@@ -2,46 +2,57 @@ function Set-Window {
 [CmdletBinding()]
     param
     (
-      [IntPtr]
-      $WindowHandle,
+      [IntPtr] $WindowHandle,
       [Pinvoke.WindowAffinity] $Affinity,
+      [Pinvoke.WindowShowStyle] $Style,
       [Pinvoke.zOrderOptions] $zOrder,
       [Pinvoke.RECT]$Coordinates
     )
 
-    if ($null -eq $Affinity){
+    if ($PSBoundParameters.ContainsKey('Affinity')){
       $Success = [Pinvoke.User32]::SetWindowDisplayAffinity($WindowHandle, $Affinity)
-    }
-    if (-not $Success){
+      if (-not $Success){
         $ErrorMessage = [System.ComponentModel.Win32Exception][System.Runtime.InteropServices.Marshal]::GetLastWin32Error()
         Write-Error $ErrorMessage
         throw "Failed setting $Affinity"
+      }
     }
 
-    if ($null -eq $zOrder){
+
+    if ($PSBoundParameters.ContainsKey('Zorder')){
       $Success = [Pinvoke.User32]::SetWindowPos($WindowHandle,$zorder,0,0,0,0, [Pinvoke.WindowOptions]::SWP_NOSIZE + [Pinvoke.WindowOptions]::SWP_NOMOVE  )
-    }
-    if (-not $Success){
+      if (-not $Success){
         $ErrorMessage = [System.ComponentModel.Win32Exception][System.Runtime.InteropServices.Marshal]::GetLastWin32Error()
         Write-Error $ErrorMessage
         throw "Failed setting $Zorder"
+      }
     }
 
-    if ($null -eq $options){
+    if ($PSBoundParameters.ContainsKey('Options')){
       $Success = [Pinvoke.User32]::SetWindowDisplayAffinity($WindowHandle, $Affinity)
+      if (-not $Success){
+        $ErrorMessage = [System.ComponentModel.Win32Exception][System.Runtime.InteropServices.Marshal]::GetLastWin32Error()
+        Write-Error $ErrorMessage
+        throw "Failed setting $Options"
+      }
     }
-    if (-not $Success){
-      $ErrorMessage = [System.ComponentModel.Win32Exception][System.Runtime.InteropServices.Marshal]::GetLastWin32Error()
-      Write-Error $ErrorMessage
-      throw "Failed setting $Options"
+    
+    if ($PSBoundParameters.ContainsKey('Options')){
+      $Success = [Pinvoke.User32]::SetWindowPos($WindowHandle,0,$rect.Left,$rect.Top,$rect.right,$rect.bottom, [Pinvoke.WindowOptions]::SWP_SHOWWINDOW  )
+      if (-not $Success){
+        $ErrorMessage = [System.ComponentModel.Win32Exception][System.Runtime.InteropServices.Marshal]::GetLastWin32Error()
+        Write-Error $ErrorMessage
+        throw "Failed setting window location"
+      }
     }
 
-    if ($null -eq $Coordinates){
-      $Success = [Pinvoke.User32]::SetWindowPos($WindowHandle,0,$rect.Left,$rect.Top,$rect.right,$rect.bottom, [Pinvoke.WindowOptions]::SWP_SHOWWINDOW  )
+    if ($PSBoundParameters.ContainsKey('Style')){
+      $Success = [Pinvoke.User32]::ShowWindow($WindowHandle,$Style )
+      if (-not $Success){
+        $ErrorMessage = [System.ComponentModel.Win32Exception][System.Runtime.InteropServices.Marshal]::GetLastWin32Error()
+        Write-Error $ErrorMessage
+        throw "Failed style $Style"
+      }
     }
-    if (-not $Success){
-      $ErrorMessage = [System.ComponentModel.Win32Exception][System.Runtime.InteropServices.Marshal]::GetLastWin32Error()
-      Write-Error $ErrorMessage
-      throw "Failed setting window location"
-    }
+
  }
