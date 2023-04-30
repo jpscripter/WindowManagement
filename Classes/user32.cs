83aad3e9196fd8a1245d1a523593f94b70a6c85a
JPScripter <JPScripter@gmail.com>
2023-04-30 12:13:07 -0500

    show

--- Classes/user32.cs
+++ Classes/user32.cs
@@ -78,6 +78,63 @@ namespace Pinvoke
         //The window is displayed only on a monitor. Everywhere else, the window does not appear at all. One use for this affinity is for windows that show video recording controls, so that the controls are not included in the capture.
         WDA_EXCLUDEFROMCAPTURE = 0x00000011
     }
+    public enum WindowShowStyle : uint
+    {
+        /// <summary>Hides the window and activates another window.</summary>
+        SW_HIDE = 0,
+
+        /// <summary>Activates and displays a window. If the window is minimized
+        /// or maximized, the system restores it to its original size and
+        /// position. An application should specify this flag when displaying
+        /// the window for the first time.</summary>
+        SW_SHOWNORMAL = 1,
+
+        /// <summary>Activates the window and displays it as a minimized window.</summary>
+        SW_SHOWMINIMIZED = 2,
+
+        /// <summary>Activates the window and displays it as a maximized window.</summary>
+        SW_SHOWMAXIMIZED = 3,
+
+        /// <summary>Maximizes the specified window.</summary>
+        SW_MAXIMIZE = 3,
+
+        /// <summary>Displays a window in its most recent size and position.
+        /// This value is similar to "ShowNormal", except the window is not
+        /// actived.</summary>
+        SW_SHOWNOACTIVATE = 4,
+
+        /// <summary>Activates the window and displays it in its current size
+        /// and position.</summary>
+        SW_SHOW = 5,
+
+        /// <summary>Minimizes the specified window and activates the next
+        /// top-level window in the Z order.</summary>
+        SW_MINIMIZE = 6,
+
+        /// <summary>Displays the window as a minimized window. This value is
+        /// similar to "ShowMinimized", except the window is not activated.</summary>
+        SW_SHOWMINNOACTIVE = 7,
+
+        /// <summary>Displays the window in its current size and position. This
+        /// value is similar to "Show", except the window is not activated.</summary>
+        SW_SHOWNA = 8,
+
+        /// <summary>Activates and displays the window. If the window is
+        /// minimized or maximized, the system restores it to its original size
+        /// and position. An application should specify this flag when restoring
+        /// a minimized window.</summary>
+        SW_RESTORE = 9,
+
+        /// <summary>Sets the show state based on the SW_ value specified in the
+        /// STARTUPINFO structure passed to the CreateProcess function by the
+        /// program that started the application.</summary>
+        SW_SHOWDEFAULT = 10,
+
+        /// <summary>Windows 2000/XP: Minimizes a window, even if the thread
+        /// that owns the window is hung. This flag should only be used when
+        /// minimizing windows from a different thread.</summary>
+        SW_FORCEMINIMIZE = 11,
+    }
     public delegate bool EnumDesktopsDelegate(string desktop, IntPtr lParam);
     public delegate bool EnumWindowProc(IntPtr hWnd, IntPtr parameter);
     public delegate bool EnumWindowStationsDelegate(string windowsStation, IntPtr lParam);
@@ -114,10 +171,14 @@ namespace Pinvoke
         public static extern IntPtr GetWindow(IntPtr hWnd, GetWindowType uCmd);
         [DllImport("user32.dll", SetLastError = true)]
         public static extern IntPtr GetDC(IntPtr hWnd);
-        [DllImport("user32.dll")]
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         public static extern bool GetWindowRect(IntPtr hWnd, out RECT hWndInsertAfter);
-        [DllImport("user32.dll")]
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, long X, long Y, long cx, long cy, uint uFlags);
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool ShowWindow(IntPtr hwnd, int show);
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool EnumChildWindows(IntPtr window, EnumWindowProc callback, IntPtr i);
