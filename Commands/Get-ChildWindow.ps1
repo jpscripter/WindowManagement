06f3d7f491ebc02656895c612f0b6d501a6b1096
JPScripter <JPScripter@gmail.com>
2024-09-12 21:02:53 -0500

    Updated windowing controls

--- Commands/Get-ChildWindow.ps1
+++ Commands/Get-ChildWindow.ps1
@@ -13,8 +13,12 @@ param (
         $Null = $Windows.add([PSCustomObject] @{
             WindowHandle = $WindowHandle
             ChildId = $child
+            ParentID = (Get-WindowParent -WindowHandle $child)
+            ChildClass = (Get-WindowClass -WindowHandle $child)
             ChildTitle = (Get-WindowName -WindowHandle $child)
             processID = Get-WindowProcess -WindowHandle $child
+            Children = (Get-ChildWindow -WindowHandle $child -All)
+            Visable = [Pinvoke.User32]::IsWindowVisible($child)
         })
 
     }
